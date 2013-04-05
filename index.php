56a84aba826cf91695aaa87fcd2eabc7c63eb1c9
danestrategas <daniel.garcia@estrategasdigitales.com>
2013-04-05 11:25:48 -0600

    Index.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('Connections/otono2011.php'); ?>
+ <?php require_once('Connections/otono2011.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
