4bd6e94637a88f9e4f60fc7f388ef5c26b197747
danestrategas <daniel.garcia@estrategasdigitales.com>
2013-07-02 16:06:10 -0500

    Se cambia la forma en que se muestra el menú en el administrador y se cambia el query de la página principal para que busque programas por periodo y no por fecha.

--- administrador/admin_dir_maestros.php
+++ administrador/admin_dir_maestros.php
@@ -121,51 +121,9 @@ function eliminar_prog(id_programa){
   </div>
   <div id="separador"></div>
   <div id="separador"></div>
-  <div id="menu_generos_interior_index">
-    <div class="roundedBox_interior_index" id="type1"> 
-      <!-- esquinas -->
-      <div class="corner topLeft"></div>
-      <div class="corner topRight"></div>
-      <div class="corner bottomLeft"></div>
-      <div class="corner bottomRight"></div>
-      <!-- esquinas -->
-      <div id="menu_desplega_index">
-        <div id="menu_areas">
-          <h2>Programas</h2>
-          <ul>
-            <li><a href="programas_home.php">Programas</a> </li>
-            <li><a href="fechas_home.php">Fechas</a> </li>
-            <li><a href="fechas_idiom_home.php">Fechas Idiomas</a> </li>
-            <li><a href="propuestas_progr_home.php">Propuestas Programas</a></li>
-          </ul>
-          <p>&nbsp;</p>
-          <h2>Carrusel Index</h2>
-          <ul>
-            <li><a href="admin_carrusel/index.php">Banners</a></li>
-          </ul>
-          <p>&nbsp;</p>
-          <h2>Art&iacute;culos</h2>
-          <ul>
-            <li><a href="admin_discipline_articles.php?id_discipline=1">Disciplinas</a> </li>
-            <li><a href="admin_opinions.php">La Comunidad Ibero Opina</a> </li>            
-            <li><a href="admin_weekly_articles.php">Art&iacute;culos semanales</a> </li>
-            <!--li><a href="admin_media_articles.php">La DEC en los Medios</a> </li-->
-          </ul>
-          <p>&nbsp;</p>
-          <h2>Sedes</h2>
-          <ul>
-            <li><a href="admin_sedes_home.php">Sedes</a></li>
-          </ul>
-          <p>&nbsp;</p>
-          <h2>Directorio</h2>
-          <ul>
-            <li><a href="admin_dir_dec.php">DEC</a></li>
-            <li><a href="admin_dir_maestros.php">Maestros</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
+<!-- LLamada a menú de secciones-->
+<?php include('menu_secciones.php'); ?>
+<!-- Termina llamada a menú de secciones-->
   <div id="contenedor_irregular_index" style="width:800px;"><!-- InstanceBeginEditable name="contenido" -->
  <h1> Directorio DEC </h1>
  <div style="heigth:80px; float:right; margin:10px 0px;">
