11da3a2c7ab1feef6e52532d078bcd511a66a486
Estrategas Digitales <jorge@estrategasdigitales.com>
2013-07-02 14:02:34 -0500

    Create config.php
    Archivo con los parámetros de configuración para la base de datos

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Archivo: config.php
+* Fecha: 28/06/2013
+* Creador: Eric Bravo para Estrategas Digitales
+* Descripcion: Archivo de configuración para Bases de Datos
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Historico
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+//Servidor
+$host = "localhost";
+
+//Usuario
+$user = "root";
+
+//Password
+$password = "root";
+
+//Base de datos
+$db = "base_de_datos";
+
+?>
