f0cbe6947999f24ad296215f38578e0aa3609d07
Adrian Guerrero <guerrero1111@hotmail.com>
2013-07-08 12:35:39 -0500

    cambio en consulta de resultados

--- resultados.php
+++ resultados.php
@@ -1,5 +1,66 @@
-<?php require_once('Connections/otono2011.php'); ?>
-<?php
+<?php require_once('Connections/otono2011.php'); 
+
+function sanear_string($string)
+{
+
+    $string = trim($string);
+
+    $string = str_replace(
+        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+        $string
+    );
+
+    $string = str_replace(
+        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+        $string
+    );
+
+    $string = str_replace(
+        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+        $string
+    );
+
+    $string = str_replace(
+        array('ñ', 'Ñ', 'ç', 'Ç'),
+        array('n', 'N', 'c', 'C',),
+        $string
+    );
+
+    //Esta parte se encarga de eliminar cualquier caracter extraño
+    $string = str_replace(
+        array("\\", "¨", "º", "-", "~",
+             "#", "@", "|", "!", "\"",
+             "·", "$", "%", "&", "/",
+             "(", ")", "?", "'", "¡",
+             "¿", "[", "^", "`", "]",
+             "+", "}", "{", "¨", "´",
+             ">", "< ", ";", ",", ":",
+             ".", ""),
+        '',
+        $string
+    );
+
+
+    return $string;
+}
+
+
+
 if (!function_exists("GetSQLValueString")) {
 function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
 {
@@ -103,8 +164,11 @@ if($_POST['search'] == "a"){
 	  AND site_programs.cancelado = 0 
 	  AND site_programs.periodo = 'o' 
 	  ORDER BY program_name ASC LIMIT 0,20";
+
 }else{
-	$query_search = "SELECT * FROM site_programs WHERE program_name LIKE '%".htmlentities($_POST['buscar'])."%' OR description LIKE '%".htmlentities($_POST['buscar'])."%' OR id_discipline IN (SELECT id_discipline FROM disciplines WHERE discipline LIKE '%".htmlentities($_POST['buscar'])."%') AND cancelado = 0 AND id_program IN (SELECT id_program FROM site_fechas_ini WHERE periodo = 'o') ORDER BY program_name ASC LIMIT 0,20";
+	$buscar=sanear_string($_POST['buscar']);
+	$query_search = "SELECT * FROM site_programs WHERE program_name LIKE '%".$buscar."%' OR description LIKE '%".$buscar."%' OR id_discipline IN (SELECT id_discipline FROM disciplines WHERE discipline LIKE '%".$buscar."%') AND cancelado = 0 AND id_program IN (SELECT id_program FROM site_fechas_ini WHERE periodo = 'o') ORDER BY program_name ASC LIMIT 0,20";
+
 }
 
 $search = mysql_query($query_search, $otono2011) or die(mysql_error());
