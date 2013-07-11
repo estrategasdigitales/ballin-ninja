8e960679d9d80d5a2d992aa2fa23b963fe1bb5b2
danestrategas <daniel.garcia@estrategasdigitales.com>
2013-07-11 13:04:47 -0500

    Cambio de tamaño de thumbnail.

--- administrador/galeria_programas_nuevo.php
+++ administrador/galeria_programas_nuevo.php
@@ -73,7 +73,7 @@ $totalRows_programa_galeria = mysql_num_rows($programa_galeria);
               $photo->save($photosDir."/".$img_filename);
 
               $photo->load($IMAGE_FILE);
-              $photo->resize(500,500);
+              $photo->resize(150,150);
               $photo->save($photosDir."thumbnails/".$img_filename);
 
               $insertSQL2 = sprintf("INSERT INTO site_archivo_galeria(id_galeria_programa, archivo) VALUES ('%s', '%s')",
