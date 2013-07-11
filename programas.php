12762a4219369a72a9c5c2fff61cc180775097e3
danestrategas <daniel.garcia@estrategasdigitales.com>
2013-07-11 09:34:10 -0500

    Cambios.

--- programas.php
+++ programas.php
@@ -635,9 +635,11 @@ s.parentNode.insertBefore(ga, s);
 					<?php } ?>
 				</ul>
 				<div class="clearfix"></div>
-				<a id="prev2" class="prev" href="#">&lt;</a>
-				<a id="next2" class="next" href="#">&gt;</a>
-				<div id="pager2" class="pager"></div>
+				<div>
+					<a id="prev2" class="prev" href="#">&lt;</a>
+					<a id="next2" class="next" href="#">&gt;</a>
+					<div id="pager2" class="pager"></div>
+				</div>
 			</div>
 				</td>
                   	</tr>
