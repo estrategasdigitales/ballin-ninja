2a9fcf0b4e28027bf4d407755f2886ab561b97e0
danestrategas <daniel.garcia@estrategasdigitales.com>
2013-07-17 16:16:47 -0500

    Se agrega el menú por medio de include de PHP.

--- community_opinions.php
+++ community_opinions.php
@@ -337,43 +337,7 @@ s.parentNode.insertBefore(ga, s);
       <!-- esquinas -->
       <!-- esquinas -->
       <div id="menu_desplega_index">
-        <div id="menu_areas">
-          <p class="header_disciplinas"></p>
-          <ul>
-            <li><a class="discipline_1" onclick="showMenu(1)">Arquitectura</a></li>
-            <li><a class="discipline_2" onclick="showMenu(2)">Arte</a></li>
-            <li><a class="discipline_3" onclick="showMenu(3)">Diseño</a></li>
-            <li><a class="discipline_7" onclick="showMenu(7)">Pol&iacute;tica y Derecho</a></li>
-            <li><a class="discipline_5" onclick="showMenu(5)">Desarrollo Humano</a></li>
-            <li><a class="discipline_6" onclick="showMenu(6)">Salud</a></li>     
-            <li><a class="discipline_8" onclick="showMenu(8)">Negocios</a></li>
-            <li><a class="discipline_9" onclick="showMenu(9)">Tecnolog&iacute;a</a></li>
-            <li><a class="discipline_10" onclick="showMenu(10)">Humanidades</a></li>
-            <li><a href="programas.php?id_discipline=18&id_program=323">Ciencias Religiosas</a></li>
-            <li><a class="discipline_11" onclick="showMenu(11)">Gastronom&iacute;a</a></li>
-            <li><a class="discipline_12" onclick="showMenu(12)">Preparatoria Abierta</a></li>
-            <li><a class="discipline_14" onclick="showMenu(14)">Idiomas</a></li>
-          </ul>
-         <h4>Programas ejecutivos</h4>
-          <ul>
-            <li><a class="discipline_23" onclick="showMenu(23)">Programas impartidos por Harvard University</a></li>
-          </ul>
-          <h4>Centros de Atención Especializada</h4>
-          <ul>
-            <li><a class="discipline_15" onclick="showMenu(15)">Ibero Online</a></li>
-            <li><a class="discipline_16" onclick="showMenu(16)">Atenci&oacute;n Integral a Empresas</a></li>
-            <li><a class="discipline_17" onclick="showMenu(17)">Atenci&oacute;n Integral al Sector P&uacute;blico</a></li>
-          </ul>
-          <h4>Sedes Externas</h4>
-          <ul>
-            <li><a class="discipline_20" onclick="showMenu(20)">Sede sur: Estudio Lofft</a></li>
-            <li><a class="discipline_13" onclick="showMenu(13)">Xochitla</a></li>
-          </ul>
-          <h4 style="padding-top: 0px;"></h4>
-          <p id="search_on" style="padding:5px"><a onclick="show_search()" style="color:#EF353C; font-weight:bold;">Busca tu programa de inter&eacute;s </p></a>
-          <h4 style="padding-top: 0px;"></h4>
-          <p style="padding:0px 5px 5px 5px;"><a href="catalogo.php"style="color:#EF353C; font-weight:bold;">Consulta cat&aacute;logo oferta primavera 2013</a></p>
-        </div>
+      <?php include('menu_disciplines.php'); ?>
       </div>
     </div>
   </div>
