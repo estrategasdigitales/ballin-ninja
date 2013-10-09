858033eabaa70113fbfd6eabcf6ea95996e101e5
guillermojoelhuerta <guillermojoel.huerta@gmail.com>
2013-10-09 17:19:58 -0500

    admin

--- Scripts/ie_detect.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($) {
-    if ($.browser.msie){
-        if ($.browser.version<=7.0){
-        	$("body").empty()
-        	$("body").load("http://www.dec-uia.com/otono_2011/Scripts/ie_detect/update_browser.html")
-        	//$("body").text("Pruebal")
-        }
-    }
-});
